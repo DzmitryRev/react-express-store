d84ceeb9d8ebd9646c0f8ad1200f049dc74217c7
unknown <revchenkodzmitry@gmail.com>
2022-01-02 16:28:52 +0300

    3.Create React App

---
+++ frontend/src/App.js
@@ -0,0 +1,58 @@
+import data from "./data";
+
+function App() {
+  return (
+    <div className="grid-container">
+      <header className="row">
+        <div>
+          <a className="brand" href="/">
+            amazon
+          </a>
+        </div>
+        <div>
+          <a href="/cart">Cart</a>
+          <a href="/signin">Sign In</a>
+        </div>
+      </header>
+      <main>
+        <div className="row center">
+          {data.products.map((product) => {
+            return (
+              <div className="card" key={product._id}>
+                <a href={`/product/${product._id}`}>
+                  <img className="medium" src={product.image} alt="product" />
+                </a>
+                <div className="card-body">
+                  <a href={`/product/${product._id}`}>
+                    <h2>{product.name}</h2>
+                  </a>
+                  <div className="rating">
+                    <span>
+                      <i className="fa fa-star"></i>
+                    </span>
+                    <span>
+                      <i className="fa fa-star"></i>
+                    </span>
+                    <span>
+                      <i className="fa fa-star"></i>
+                    </span>
+                    <span>
+                      <i className="fa fa-star"></i>
+                    </span>
+                    <span>
+                      <i className="fa fa-star"></i>
+                    </span>
+                  </div>
+                  <div className="price">${product.price}</div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </main>
+      <footer className="row center">All rigth reserved</footer>
+    </div>
+  );
+}
+
+export default App;
